2a9e21e678a49c76affe2eba3f5c17ce72be35d3
Viktor Sandström <Viktor Sandstrm>
2025-12-03 07:49:08 +0100

    comments

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -6,23 +6,25 @@ fn main() {
     format!("day02/{}", args().nth(1).unwrap_or("test".to_string()))).expect("no such input");
 
   println!("{}",
-    input.split(',').fold(0, |mut sum, range| {
-        range
-        .trim()
-        .split("-")
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>()
-        .chunks(2)
-        .for_each(|x| {
-          for num in x[0].parse::<u64>().unwrap()..=x[1].parse().unwrap() {
-            let len = num_len(&num);
-            let low = num % (u64::pow(10, len / 2));
-            let high = num / (u64::pow(10, len/2));
-            if low == high {
-              sum += num;
-            }
+    input
+    .split(',') // splitta varje range
+    .fold(0, |mut sum, range| { // loopa och accumulate varje range
+      range
+      .trim() // trimma whitespace ifall det finns
+      .split("-") // splitta de två värdena
+      .map(|s| s.parse::<u64>().unwrap()) // konvertera till STOR int
+      .collect::<Vec<u64>>() // samla alla (2) värdena
+      .chunks(2) // bunta ihop 2 och 2
+      .for_each(|x| { // iterera över paren
+        for num in x[0]..=x[1] { // för varje nummer i rangen
+          let len = num_len(&num); // nummerlängden
+          let low = num % (u64::pow(10, len / 2)); // extrahera botten
+          let high = num / (u64::pow(10, len/2)); // extrahera toppen
+          if low == high { // jämför
+            sum += num;
           }
-        });
+        }
+      });
       sum
     })
   );
