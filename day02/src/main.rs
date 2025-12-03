5a64d2f347ff0c4d98541661329a4c9802a8b898
Viktor Sandström <Viktor Sandstrm>
2025-12-03 02:04:41 +0100

    day02 part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -5,28 +5,30 @@ fn main() {
   let input = std::fs::read_to_string(
     format!("day02/{}", args().nth(1).unwrap_or("test".to_string()))).expect("no such input");
 
-  // Day2 part 1:
-  let mut invalid_sum = 0;
-  input.split(',').for_each(|l| {
-    let [ref a, ref b] = l
-      .trim()
-      .split("-")
-      .map(|s| s.to_string())
-      .collect::<Vec<String>>()[..] else { println!("{l}"); panic!() };
+  println!("{}",
+    input.split(',').fold(0, |mut sum, r| {
+        r
+        .trim()
+        .split("-")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+        .chunks(2)
+        .for_each(|x| {
+          for num in x[0].parse::<u64>().unwrap()..=x[1].parse().unwrap() {
+            let len = num_len(&num);
+            if !len.is_multiple_of(2) { continue; }
+            let low = num % (u64::pow(10, len / 2));
+            let high = num / (u64::pow(10, len/2));
+            if low == high {
+              sum += num;
+            }
+          }
 
-    let (low, high) = (a.parse::<u64>().unwrap(), b.parse::<u64>().unwrap());
+        });
 
-    'inner: for n in low..=high {
-      let len = num_len(&n);
-      if !len.is_multiple_of(2) { continue 'inner; }
-      let low = n % (u64::pow(10, len / 2));
-      let high = n / (u64::pow(10, len/2));
-      if low == high {
-        invalid_sum += n;
-      }
-    }
-  });
-  println!("{invalid_sum}");
+      sum
+    })
+  );
 }
 
 #[inline(always)]
@@ -38,28 +40,4 @@ fn num_len(num: &u64) -> u32 {
     num /= 10;
   }
   i
-}
-
-#[inline(always)]
-fn dub_num(num: &u64) -> u64 {
-  let len = num_len(num);
-  num + num * (u64::pow(10, len))
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test] 
-  fn half() {
-    let x = 1234;
-    let n = num_len(&x);
-    assert_eq!(34, x % (u64::pow(10, n / 2)));
-  }
-
-  #[test] 
-  fn double() {
-    let x = 34;
-    assert_eq!(3434, dub_num(&x));
-  }
 }
