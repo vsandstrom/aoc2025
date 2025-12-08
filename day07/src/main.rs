d605323c757e4ca0bdf974e49aab0d9b310465da
Viktor Sandström <Viktor Sandstrm>
2025-12-08 16:31:50 +0100

    day07 part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -3,23 +3,17 @@ use std::collections::HashSet;
 use std::{cmp, include_str};
 
 fn main() {
-  // let input = std::fs::read_to_string( format!("{}/{}", var("CARGO_MANIFEST_DIR").unwrap(), &args().nth(1).unwrap_or("test".to_string()))).expect("no such input"); 
-  //
-  // let path = format!("{}/{}", var("CARGO_MANIFEST_DIR").unwrap(), "test");
   let test = args().nth(1).unwrap_or("test".to_string());
   let input = match test.as_str() {
-    "test" | "" => include_str!("../../day07/test"),
-    "input" => include_str!("../../day07/input"),
+    "test" | "" => include_str!("../../day07/test"), // test och
+    "input" => include_str!("../../day07/input"),    // input inkluderad i binary;
     _ => panic!()
   };
 
 
   let len = input.lines().next().unwrap().len();
   let mut start = vec![false; len];
-  let mut bp = vec!();
-
-
-  input.lines().for_each(|l| {
+  let bp = input.lines().fold(vec!(), |mut arr, l| {
     let mut bps = vec![false; len];
     l.chars().enumerate().for_each(|(j, c)| {
       match c {
@@ -29,7 +23,8 @@ fn main() {
         _ => unreachable!()
       };
     });
-    bp.push(bps);
+    arr.push(bps);
+    arr
   });
 
   println!("{}",
@@ -37,7 +32,7 @@ fn main() {
       let mut temp = vec![false; len];
       sum += start.iter().enumerate().fold(0, |sum, (i, beam)| {
         sum + if row[i] && *beam {
-          if i != 0 && let Some(b) = temp.get_mut(i-1) { *b = true;}
+          if let Some(b) = temp.get_mut(i-1) { *b = true;}
           if let Some(b) = temp.get_mut(i+1) { *b = true;}
           1
         } else {
